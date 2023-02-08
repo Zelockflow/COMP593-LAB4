59f107418e08d95516305090f1d2cf1fe33b097a
Michael <mipadhiy@flemingcollege.ca>
2023-02-08 14:50:49 -0500

    followed up to step 10 must fix some errors

--- lab4.py
+++ lab4.py
@@ -1,33 +1,30 @@
-
+from loganalysis import get_log_file_from_cmd_line, Filter_log_by_regex
 def main():
-    log_file = get_log_file_path_from_cmd_line()
+    log_file = get_log_file_path_from_cmd_line(1)
+    port_traffic = tally_port_traffic(log_file)
+    
+    for port_num, count in port_traffic.items():
+        if count >= 100:
+            generate_port_traffic_report(log_file, port_num)
 
-# TODO: Step 3
-def get_log_file_path_from_cmd_line():
-    return
-
-# TODO: Steps 4-7
-def filter_log_by_regex(log_file, regex, ignore_case=True, print_summary=False, print_records=False):
-    """Gets a list of records in a log file that match a specified regex.
-
-    Args:
-        log_file (str): Path of the log file
-        regex (str): Regex filter
-        ignore_case (bool, optional): Enable case insensitive regex matching. Defaults to True.
-        print_summary (bool, optional): Enable printing summary of results. Defaults to False.
-        print_records (bool, optional): Enable printing all records that match the regex. Defaults to False.
-
-    Returns:
-        (list, list): List of records that match regex, List of tuples of captured data
-    """
-    return
 
 # TODO: Step 8
 def tally_port_traffic(log_file):
-    return
+    data = filter_log_by_regex(log_file, r'DPT=(.+?)')[1]
+    port_traffic = {}
+    for d in data:
+        port = d[0]
+        port_traffic[port] = port_traffic.get(port, 0) + 1
+    return port_traffic
 
 # TODO: Step 9
 def generate_port_traffic_report(log_file, port_number):
+    regex = r'(.{6}) (.{8}) .*SRC=(.+) DST=(.+?) .+SPT=(.+)' + f 'DPT=({port_number})'
+    data = filter_log_by_regex(log_file, regex)[1]
+
+    report_df = pd.DataFrame(data)
+    header_row = ('Date', 'Time', 'Source IP Address', 'Source Port', 'Destination Port')
+    report_df.to_csv(f'destination_port_{port_number}_report.csv', index=False)
     return
 
 # TODO: Step 11
